943d388cafe9131a08772384f3c5612272ee4380
rasparkle7-boop <rasparkle7@gmail.com>
2026-07-05 02:56:19 +0545

    Use absolute path with process.cwd() for ESM

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 module.exports = async (req, res) => {
-  const myApp = await import('../artifacts/api-server/dist/index.mjs');
+  const path = require('path');
+  const myApp = await import(path.join(process.cwd(), 'artifacts/api-server/dist/index.mjs'));
   return (myApp.default || myApp)(req, res);
 };
