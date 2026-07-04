8ffcdcb4977788154d2d92fdb193c3a8e1b78aed
rasparkle7-boop <rasparkle7@gmail.com>
2026-07-05 03:12:16 +0545

    Move app logic to api/appcode to bypass cache

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 module.exports = async (req, res) => {
-  const myApp = await import('./dist/index.mjs');
+  const myApp = await import('./appcode/index.mjs');
   return (myApp.default || myApp)(req, res);
 };
